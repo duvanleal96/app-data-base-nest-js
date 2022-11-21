425db54305ceb22717ea192d703c64edc0e99098
duvan leal <duvanleal65@gmail.com>
2022-11-21 01:35:55 -0500

    servicio y controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FacturaDto } from './dto/factura.dto';
+import { FacturaEntity } from './entities/factura.entity';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  createFactura(@Body() factura: FacturaDto): Promise<FacturaEntity> {
+    const newFactura = new FacturaEntity(factura);
+    return this.appService.create(newFactura);
   }
 }
